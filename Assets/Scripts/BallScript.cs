08097ae70d34a397573e398566c5216db85d48dd
Macks0 <94944942+Macks0@users.noreply.github.com>
2025-01-23 22:30:05 -0600

    first commit

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+
+    // Vars
+    protected Rigidbody rb;
+    [SerializeField] protected float worldVelocity = 25;
+    [SerializeField] protected Vector3 startForce = Vector3.one;
+
+    // Awake
+
+
+    // Methods
+
+    private void OnEnable()
+    {
+        rb = GetComponent<Rigidbody>();
+        ApplyStartForce();
+    }
+    private void FixedUpdate()
+    {
+        LimitBallVelocity();
+    }
+    
+    protected void ApplyStartForce()
+    {
+        rb.AddForce(startForce, ForceMode.Impulse);
+    }
+    protected void LimitBallVelocity()
+    {
+        Vector3 hV3 = rb.velocity;
+        Vector3 nV3 = Vector3.zero;
+
+        nV3.x = Mathf.Clamp(hV3.x, -worldVelocity, worldVelocity);
+        nV3.y = Mathf.Clamp(hV3.y, -worldVelocity, worldVelocity);
+
+        rb.velocity = nV3;
+
+    }
+
+}
